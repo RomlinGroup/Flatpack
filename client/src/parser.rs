4bf225623d277051b3e603da52fe9f9e7eddf993
Romlin Group AB <hello@romlin.com>
2023-05-24 21:10:29 +0200

    Testing debian:stable-slim

--- client/src/parser.rs
+++ client/src/parser.rs
@@ -41,7 +41,7 @@ pub async fn parse_toml_to_dockerfile(url: &str) -> Result<String, Box<dyn Error
     dockerfile.push_str("# It is not intended for manual editing.\n\n");
 
     // Base image
-    dockerfile.push_str(&format!("FROM {}\n\n", config.base_image));
+    dockerfile.push_str(&format!("FROM {}\n", config.base_image));
 
     // Create directories
     dockerfile.push_str("\n# Create directories\n");
