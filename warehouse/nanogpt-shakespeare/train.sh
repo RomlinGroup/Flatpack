315c60372c96617c036cbd0e506ca952b10f822d
Romlin Group AB <hello@romlin.com>
2023-06-08 01:30:20 +0200

    Simplified train.sh

--- warehouse/nanogpt-shakespeare/train.sh
+++ warehouse/nanogpt-shakespeare/train.sh
@@ -2,9 +2,13 @@
 
 OS=$(uname)
 if [ "$OS" = "Darwin" ]; then
+  WORK_DIR="nanoGPT"
   DEVICE="mps"
 else
+  WORK_DIR="/home/content/nanoGPT"
   DEVICE="cpu"
 fi
+
+cd "$WORK_DIR" || exit
 
 python train.py config/train_shakespeare_char.py --device=$DEVICE --compile=False
