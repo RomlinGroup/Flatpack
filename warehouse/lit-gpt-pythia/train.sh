ee32d022da447ccb185bffbdbc17a299a76d12a5
Romlin Group AB <hello@romlin.com>
2023-07-19 20:01:16 +0200

    Just work already

--- warehouse/lit-gpt-pythia/train.sh
+++ warehouse/lit-gpt-pythia/train.sh
@@ -27,4 +27,4 @@ python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/EleutherAI/
 sed -i 's#DATA_FILE_URL = "https://raw.githubusercontent.com/tloen/alpaca-lora/main/alpaca_data_cleaned_archive.json"#DATA_FILE_URL = "https://raw.githubusercontent.com/romlingroup/OpenAlpaca/main/openalpaca.json"#' scripts/prepare_alpaca.py
 sed -i 's#DATA_FILE_NAME = "alpaca_data_cleaned_archive.json"#DATA_FILE_NAME = "openalpaca.json"#' scripts/prepare_alpaca.py
 python scripts/prepare_alpaca.py --checkpoint_dir checkpoints/EleutherAI/pythia-1b
-python finetune/adapter.py --checkpoint_dir checkpoints/EleutherAI/pythia-1b
+python finetune/adapter.py --checkpoint_dir checkpoints/EleutherAI/pythia-1b --precision 32-true
