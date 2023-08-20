d109556086a42700d7766a6a7b7b53b70c2ef683
Romlin Group AB <hello@romlin.com>
2023-08-20 22:06:26 +0200

    Updated package

--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="0.0.9",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "requests",
