dd947ed6efd6f752dae3d0394990e7d5d4fd71f2
Romlin Group AB <hello@romlin.com>
2023-09-25 12:28:31 +0200

    Testing the train function

--- package/flatpack/flatpack/train.py
+++ package/flatpack/flatpack/train.py
@@ -13,7 +13,7 @@ def train(train_function, save_dir, model_type='rnn', framework='pytorch', *args
     epochs = kwargs.get('epochs', 10)
     batch_size = kwargs.get('batch_size', 32)
 
-    print(f"Training {model_type} model with epochs: {epochs} and batch size: {batch_size}")
+    print(f"🚀 Training {model_type} model with epochs: {epochs} and batch size: {batch_size}")
 
     # Initialize a timer
     start_time = time.time()
@@ -23,16 +23,16 @@ def train(train_function, save_dir, model_type='rnn', framework='pytorch', *args
         output = train_function(*args, **kwargs)
 
         # Print loss and accuracy at the end of each epoch
-        print(f"Epoch {epoch + 1}/{epochs}, Loss: {output['loss']:.4f}, Accuracy: {output['accuracy']:.4f}")
+        print(f"📊 Epoch {epoch + 1}/{epochs}, Loss: {output['loss']:.4f}, Accuracy: {output['accuracy']:.4f}")
 
         # Calculate and print the elapsed time
         elapsed_time = time.time() - start_time
-        print(f"Elapsed Time: {elapsed_time:.2f} seconds")
+        print(f"⏱️ Elapsed Time: {elapsed_time:.2f} seconds")
 
     # Calculate and display the total training time
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Training completed in {elapsed_time:.2f} seconds")
+    print(f"✅ Training completed in {elapsed_time:.2f} seconds")
 
     # Save the trained model based on the specified framework
     if framework == 'pytorch':
--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="0.2.3",
+    version="0.2.4",
     packages=find_packages(),
     install_requires=[
         "requests",
