6e828ddbc47e3ca282525ebe190c56770d1b0fa0
Romlin Group AB <hello@romlin.com>
2023-09-25 11:24:26 +0200

    Testing the train function

--- package/flatpack/flatpack/train.py
+++ package/flatpack/flatpack/train.py
@@ -1,10 +1,8 @@
-# flatpack/train.py
-
 import os
 from tqdm import tqdm
 
 
-def train(train_function, save_dir, framework='pytorch', *args, **kwargs):
+def train(train_function, save_dir, model_type='rnn', framework='pytorch', *args, **kwargs):
     """
     A universal training function provided by flatpack.
     """
@@ -14,7 +12,7 @@ def train(train_function, save_dir, framework='pytorch', *args, **kwargs):
     epochs = kwargs.get('epochs', 10)
     batch_size = kwargs.get('batch_size', 32)
 
-    print(f"Training model with epochs: {epochs} and batch size: {batch_size}")
+    print(f"Training {model_type} model with epochs: {epochs} and batch size: {batch_size}")
 
     # Initialize a progress bar using tqdm
     with tqdm(total=epochs, desc='Training', unit='epoch') as pbar:
@@ -25,8 +23,10 @@ def train(train_function, save_dir, framework='pytorch', *args, **kwargs):
             pbar.set_postfix(loss=output.get('loss', 'N/A'))
             pbar.update(1)
 
+    # Save the trained model based on the specified framework
     if framework == 'pytorch':
         import torch
-        torch.save(output['model'].state_dict(), os.path.join(save_dir, 'model.pth'))
+        # Save the model with a filename based on the model_type
+        torch.save(output['model'].state_dict(), os.path.join(save_dir, f'{model_type}_model.pth'))
 
     return output
