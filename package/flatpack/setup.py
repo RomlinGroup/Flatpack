b4ff87ece6fc431c90daaaa2876f9fb1f7330d2d
Romlin Group AB <hello@romlin.com>
2023-09-25 11:44:23 +0200

    Testing the train function

--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="0.1.9",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "requests",
