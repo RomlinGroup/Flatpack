c37484ce4e0d18acd8cca21e085e5bd18b0860d6
Romlin Group AB <hello@romlin.com>
2023-09-28 01:22:43 +0200

    Refactoring modules

--- package/flatpack/flatpack/modules/transformer.py
+++ package/flatpack/flatpack/modules/transformer.py
@@ -1,116 +1,18 @@
-import os
-import json
-import torch
+from base import Base
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from flatpack.datasets import TextDataset
 
 
-class Transformer(nn.Module):
+class Transformer(Base):
     def __init__(self, d_model, nhead, num_encoder_layers, num_decoder_layers, vocab_size=None):
-        super(Transformer, self).__init__()
-        self.d_model = d_model
+        super(Transformer, self).__init__(d_model, vocab_size)
         self.nhead = nhead
         self.num_encoder_layers = num_encoder_layers
         self.num_decoder_layers = num_decoder_layers
-        self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(self.vocab_size, self.d_model) if vocab_size is not None else None
         self.transformer = nn.Transformer(d_model, nhead, num_encoder_layers, num_decoder_layers)
-        self.fc = nn.Linear(self.d_model, self.vocab_size) if vocab_size is not None else None
-
-    @staticmethod
-    def load_torch_model(model_path):
-        return torch.load(model_path)
-
-    def load_vocab_size(self, save_dir):
-        with open(os.path.join(save_dir, 'char_to_index.json'), 'r') as f:
-            char_to_index = json.load(f)
-        self.vocab_size = len(char_to_index)
-        self.embedding = nn.Embedding(self.vocab_size, self.d_model)
-        self.fc = nn.Linear(self.d_model, self.vocab_size)
 
     def forward(self, src, tgt):
-        if self.embedding is None or self.fc is None:
-            raise ValueError("vocab_size is not loaded")
         src = self.embedding(src)
         tgt = self.embedding(tgt)
         out = self.transformer(src, tgt)
         out = self.fc(out)
         return out
-
-    @classmethod
-    def train_model(cls, indexed_text, seq_length, vocab_size, d_model, nhead, num_encoder_layers, num_decoder_layers,
-                    epochs, batch_size, device):
-        dataset = TextDataset(indexed_text, seq_length=seq_length)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-        model = cls(d_model=d_model, nhead=nhead, num_encoder_layers=num_encoder_layers,
-                    num_decoder_layers=num_decoder_layers, vocab_size=vocab_size)
-        model.to(device)
-
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-        for epoch in range(epochs):
-            total_loss = 0.0
-            total_accuracy = 0.0
-            total_batches = 0
-
-            for inputs, targets in dataloader:
-                inputs = inputs.to(device)
-                targets = targets.to(device)
-                outputs = model(inputs, inputs)
-                loss = criterion(outputs.view(-1, vocab_size), targets.view(-1))
-
-                _, predicted = torch.max(outputs.data, 2)
-                correct = (predicted == targets)
-                accuracy = correct.sum().item() / (targets.size(0) * targets.size(1))
-
-                optimizer.zero_grad()
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
-                optimizer.step()
-
-                total_loss += loss.item()
-                total_accuracy += accuracy
-                total_batches += 1
-
-            average_loss = total_loss / total_batches
-            average_accuracy = total_accuracy / total_batches
-            print(f"Epoch {epoch + 1}/{epochs}, Loss: {average_loss:.4f}, Accuracy: {average_accuracy:.4f}")
-
-        return {'model': model}
-
-    def generate_text(self, save_dir, start_sequence="To be, or not to be", generate_length=1024, device=None):
-        with open(os.path.join(save_dir, 'char_to_index.json'), 'r') as f:
-            char_to_index = json.load(f)
-
-        with open(os.path.join(save_dir, 'index_to_char.json'), 'r') as f:
-            index_to_char = json.load(f)
-
-        input_sequence = [char_to_index[char] for char in start_sequence]
-        input_tensor = torch.tensor(input_sequence).long().unsqueeze(0)
-
-        if device is not None:
-            input_tensor = input_tensor.to(device)
-
-        generated_text = start_sequence
-
-        self.eval()
-
-        with torch.no_grad():
-            for _ in range(generate_length):
-                output = self(input_tensor, input_tensor)
-                probabilities = torch.nn.functional.softmax(output[0, -1], dim=0)
-                next_index = torch.multinomial(probabilities, 1).item()
-                next_token = index_to_char[str(next_index)]
-
-                generated_text += next_token
-                input_sequence = input_sequence[1:] + [next_index]
-                input_tensor = torch.tensor(input_sequence).long().unsqueeze(0)
-
-                if device is not None:
-                    input_tensor = input_tensor.to(device)
-
-        return generated_text
