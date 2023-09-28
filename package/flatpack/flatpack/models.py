390a7bd95ee73567bba362f5f79633f6615c991e
Romlin Group AB <hello@romlin.com>
2023-09-29 00:28:13 +0200

    Removed Transformer

--- package/flatpack/flatpack/models.py
+++ package/flatpack/flatpack/models.py
@@ -1,3 +1,2 @@
 from .modules.lstm import LSTM
 from .modules.rnn import RNN
-from .modules.transformer import Transformer
--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="1.2.2",
+    version="1.2.5",
     packages=find_packages(),
     install_requires=[
         "requests",
