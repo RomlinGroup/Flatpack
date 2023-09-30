fa1f38e731fc1f3b8924c96ccc1b5713dea20350
Romlin Group AB <hello@romlin.com>
2023-09-30 22:19:54 +0200

    Updated gpt2-knowledge-distillation

--- warehouse/gpt2-knowledge-distillation/train.sh
+++ warehouse/gpt2-knowledge-distillation/train.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
 # === BEGIN USER CUSTOMIZATION ===
-export REPO_NAME=GPT2-Knowledge-Distillation
-export FLATPACK_NAME=gpt2-knowledge-distillation
+REPO_NAME="GPT2-Knowledge-Distillation"
+FLATPACK_NAME="gpt2-knowledge-distillation"
 # === END USER CUSTOMIZATION ===
 
-source ./device.sh || { echo "Error: Failed to source device.sh" >&2; exit 1; }
+# Ensure logs directory exists
+mkdir -p logs
 
-# === BEGIN USER CUSTOMIZATION ===
-python data/shakespeare/prepare.py
-bash run_adamw/train_student.sh
-# === END USER CUSTOMIZATION ===
+# Function to log with timestamps
+log() {
+  echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1" >>./logs/output.log
+}
+
+# Check for required commands
+for cmd in "python" "bash"; do
+  command -v $cmd >/dev/null 2>&1 || {
+    log "$cmd is required but it's not installed."
+    exit 1
+  }
+done
+
+log "Starting script..."
+
+{
+  # Source device script
+  source ./device.sh
+
+  # === BEGIN USER CUSTOMIZATION ===
+  python data/shakespeare/prepare.py
+  bash run_adamw/train_student.sh
+  # === END USER CUSTOMIZATION ===
+
+} >>./logs/output.log 2>>./logs/error.log || log "An error occurred!"
