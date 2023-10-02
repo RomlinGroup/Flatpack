5049ff2cf5543d79eba06faddcc8388a08c26e18
Romlin Group AB <hello@romlin.com>
2023-10-02 15:57:44 +0200

    Updated gpt2-knowledge-distillation

--- warehouse/gpt2-knowledge-distillation/train.sh
+++ warehouse/gpt2-knowledge-distillation/train.sh
@@ -5,14 +5,17 @@ CURRENT_DIR=$(pwd)
 
 echo -e "🚀 train.sh is running in: $CURRENT_DIR\n"
 
+# === BEGIN USER CUSTOMIZATION ===
+export REPO_NAME=GPT2-Knowledge-Distillation
+export FLATPACK_NAME=gpt2-knowledge-distillation
+# === END USER CUSTOMIZATION ===
+
 source ./device.sh || {
   echo "⚠️ Error: Failed to source device.sh" >&2
   exit 1
 }
 
 # === BEGIN USER CUSTOMIZATION ===
-export REPO_NAME=GPT2-Knowledge-Distillation
-export FLATPACK_NAME=gpt2-knowledge-distillation
 python data/shakespeare/prepare.py
 bash run_adamw/train_student.sh
 # === END USER CUSTOMIZATION ===
