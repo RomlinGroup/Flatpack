5be4ce3698882801971d8fcf53ba497fc746d8fe
Romlin Group AB <hello@romlin.com>
2023-10-23 19:44:54 +0200

    Updated script

--- utilities/update_device/update_device.sh
+++ utilities/update_device/update_device.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 set -e
 set -u
+
+BASE_DIR="$(dirname $(dirname $(dirname $(realpath $0))))"
+SOURCE_FILE="${BASE_DIR}/warehouse/template/device.sh"
+
+if [[ ! -f ${SOURCE_FILE} ]]; then
+  echo "Error: Source file ${SOURCE_FILE} does not exist."
+  exit 1
+fi
+
+for dir in ${BASE_DIR}/warehouse/*/; do
+  if [[ "${dir}" != "${BASE_DIR}/warehouse/template/" ]]; then
+    echo "Copying ${SOURCE_FILE} to ${dir}"
+    cp "${SOURCE_FILE}" "${dir}"
+  fi
+done
+
+echo "Update completed."
