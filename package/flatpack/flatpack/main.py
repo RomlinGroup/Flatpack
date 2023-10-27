377db4d515a64993c8fc2f9bfa66b2a7757362d8
Romlin Group AB <hello@romlin.com>
2023-10-27 13:20:28 +0200

    Updated main.py

--- package/flatpack/flatpack/main.py
+++ package/flatpack/flatpack/main.py
@@ -73,15 +73,15 @@ def fpk_colorize(text, color):
         str: Colorized text.
     """
     colors = {
-        "red": "\033[91m",
-        "green": "\033[92m",
-        "yellow": "\033[93m",
         "blue": "\033[94m",
-        "magenta": "\033[95m",
         "cyan": "\033[96m",
-        "white": "\033[97m",
+        "default": "\033[0m",
+        "green": "\033[92m",
         "grey": "\033[90m",
-        "default": "\033[0m"
+        "magenta": "\033[95m",
+        "red": "\033[91m",
+        "white": "\033[97m",
+        "yellow": "\033[93m"
     }
     return colors[color] + text + colors["default"]
 
