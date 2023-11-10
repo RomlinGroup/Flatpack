93651c1f3d93b2ba12e8fbb4ae6be99d8ef2dec2
Romlin Group AB <hello@romlin.com>
2023-11-10 23:42:24 +0100

    Added llama-cpp-python

---
+++ warehouse/llama-cpp-python/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo -e "🚀 train.sh is running in: $SCRIPT_DIR\n"
+
+# === BEGIN USER CUSTOMIZATION ===
+export REPO_NAME=llama-cpp-python
+export FLATPACK_NAME=llama-cpp-python
+# === END USER CUSTOMIZATION ===
+
+source "$SCRIPT_DIR/device.sh" || {
+  echo "⚠️ Error: Failed to source device.sh" >&2
+  exit 1
+}
+
+# === BEGIN USER CUSTOMIZATION ===
+"${VENV_PYTHON}" train.py
+# === END USER CUSTOMIZATION ===
