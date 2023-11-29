d259864b49c848232647b7a56fb533a652e3570b
Romlin Group AB <hello@romlin.com>
2023-11-29 09:43:24 +0100

    Updated rwkv-infctx-trainer

--- warehouse/rwkv-infctx-trainer/train.sh
+++ warehouse/rwkv-infctx-trainer/train.sh
@@ -36,10 +36,7 @@ unzip ../zip/enwik8.zip
 mv enwik8 enwik8.txt
 ls -lh
 
-pwd
-cd ../../RWKV-v4neo || exit
-pwd
-ls
-#"${VENV_PYTHON}" preload_datapath.py notebook/dataset-config/example-local-text.yaml
-#"${VENV_PYTHON}" lightning_trainer.py fit -c notebook/dataset-config/example-local-text.yaml
+cd ../../../RWKV-v4neo || exit
+"${VENV_PYTHON}" preload_datapath.py notebook/dataset-config/example-local-text.yaml
+"${VENV_PYTHON}" lightning_trainer.py fit -c notebook/dataset-config/example-local-text.yaml
 # === END USER CUSTOMIZATION ===
