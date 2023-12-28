f1d829b25578398e370c00858eb6273869874740
Romlin Group AB <hello@romlin.com>
2023-12-28 15:30:12 +0100

    Debugging

--- simulators/cobot/object_detection/export/distribute.py
+++ simulators/cobot/object_detection/export/distribute.py
@@ -5,6 +5,7 @@ import zipfile
 
 
 def distribute_images_and_labels(base_image_folder, train_json, val_json, output_root):
+    # Load the JSON data
     with open(train_json, 'r') as file:
         train_data = json.load(file)
     with open(val_json, 'r') as file:
@@ -15,16 +16,15 @@ def distribute_images_and_labels(base_image_folder, train_json, val_json, output
 
     train_dir = os.path.join(output_root, 'train/images')
     val_dir = os.path.join(output_root, 'val/images')
-
     os.makedirs(train_dir, exist_ok=True)
     os.makedirs(val_dir, exist_ok=True)
 
     for image_name in os.listdir(base_image_folder):
         source_path = os.path.join(base_image_folder, image_name)
         if image_name in train_images:
-            shutil.move(source_path, os.path.join(train_dir, image_name))
+            shutil.copy(source_path, os.path.join(train_dir, image_name))
         elif image_name in val_images:
-            shutil.move(source_path, os.path.join(val_dir, image_name))
+            shutil.copy(source_path, os.path.join(val_dir, image_name))
 
     shutil.copy(train_json, os.path.join(output_root, 'train/label.json'))
     shutil.copy(val_json, os.path.join(output_root, 'val/label.json'))
