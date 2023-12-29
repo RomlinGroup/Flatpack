3330cb2eafaa4ecaf0d84334bc6ce5f64c26189d
Romlin Group AB <hello@romlin.com>
2023-12-29 20:09:54 +0100

    Debugging package

--- package/flatpack/flatpack/main.py
+++ package/flatpack/flatpack/main.py
@@ -547,7 +547,7 @@ async def process(prompt: str, file: UploadFile = File(None)):
 
 
 @app.post("/process_depth_map/")
-async def process_depth_map(file: UploadFile = File(...), model_type: str = "DPT_Large"):
+async def process_depth_map(file: UploadFile = File(...), model_type: str = "MiDaS_small"):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents))
     image_np = np.array(image.convert("RGB"))
@@ -558,7 +558,7 @@ async def process_depth_map(file: UploadFile = File(...), model_type: str = "DPT
     return StreamingResponse(io.BytesIO(encoded_img.tobytes()), media_type="image/png")
 
 
-def fpk_process_depth_map_np(image_np: np.ndarray, model_type: str = "DPT_Large") -> np.ndarray:
+def fpk_process_depth_map_np(image_np: np.ndarray, model_type: str = "MiDaS_small") -> np.ndarray:
     global midas_model, midas_transforms
 
     start_time = time.time()
