02e0b559d0cbd86196f5f1e97a64e5a2c1f60496
Romlin Group AB <hello@romlin.com>
2023-12-30 10:00:29 +0100

    Updated package

--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="3.1.131",
+    version="3.1.132",
     license="Apache Software License (Apache-2.0)",
     packages=find_packages(),
     install_requires=[
