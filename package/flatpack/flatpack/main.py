ca9dd044c7d0e7a3328bd311846c9ab7726d9ab4
Romlin Group AB <hello@romlin.com>
2023-12-31 21:14:05 +0100

    Removed PIL

--- package/flatpack/flatpack/main.py
+++ package/flatpack/flatpack/main.py
@@ -475,9 +475,7 @@ def fpk_generate_text_with_image(prompt, image_np):
         processor = AutoProcessor.from_pretrained(model_name)
         model = AutoModelForCausalLM.from_pretrained(model_name)
 
-        image = Image.fromarray(image_np)
-
-        pixel_values = processor(images=image, return_tensors="pt").pixel_values
+        pixel_values = processor(images=image_np, return_tensors="pt").pixel_values
         generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
         generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
