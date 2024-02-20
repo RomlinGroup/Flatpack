25bb5e09c6c1fa203e991709917c7145a195262e
Romlin Group AB <hello@romlin.com>
2024-02-20 11:53:18 +0100

    Updated flatpacks

--- warehouse/nanogpt-scratch/device.sh
+++ warehouse/nanogpt-scratch/device.sh
@@ -27,6 +27,7 @@ if [[ -d "/content" ]]; then
 elif [ "$OS" = "Darwin" ]; then
   echo "🍎 Detected macOS environment"
   export VENV_PYTHON="${SCRIPT_DIR}/bin/python"
+  DEFAULT_PATH="~"
   WORK_DIR="$DEFAULT_PATH/$FLATPACK_NAME/$REPO_NAME"
   DEVICE="mps"
 elif [ "$OS" = "Linux" ]; then
