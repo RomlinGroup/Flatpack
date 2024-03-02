74ccddb34f36bc621a6eedc4d245822ef69ff35d
Romlin Group AB <hello@romlin.com>
2024-03-02 20:21:52 +0100

    Updated obsidian-multi-modal

--- warehouse/obsidian-multi-modal/train.sh
+++ warehouse/obsidian-multi-modal/train.sh
@@ -34,4 +34,9 @@ if [[ "$OS" = "Darwin" ]]; then
 fi
 
 "${VENV_PIP}" uninstall bitsandbytes -y
+
+"${VENV_PYTHON}" -m llava.serve.cli \
+    --model-path liuhaotian/llava-v1.5-7b \
+    --image-file "https://llava-vl.github.io/static/images/view.jpg" \
+    --load-4bit
 # === END USER CUSTOMIZATION ===
