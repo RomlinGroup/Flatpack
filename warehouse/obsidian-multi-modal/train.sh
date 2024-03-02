22fe01a0a1619c21259f457d77b26e399e2e5540
Romlin Group AB <hello@romlin.com>
2024-03-02 15:56:20 +0100

    Updated obsidian-multi-modal

--- warehouse/obsidian-multi-modal/train.sh
+++ warehouse/obsidian-multi-modal/train.sh
@@ -32,4 +32,6 @@ if [[ "$OS" = "Darwin" ]]; then
   # https://developer.apple.com/metal/pytorch/
   "${VENV_PIP}" install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 fi
+
+"${VENV_PIP}" uninstall bitsandbytes
 # === END USER CUSTOMIZATION ===
