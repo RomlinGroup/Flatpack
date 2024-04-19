33c8366adb630496b95fc0420b8c92e804847cc3
Romlin Group AB <hello@romlin.com>
2024-04-20 01:37:46 +0200

    vector-add-wikipedia-page

--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="3.3.4",
+    version="3.3.5",
     license="Apache Software License (Apache-2.0)",
     packages=find_packages(),
     install_requires=[
