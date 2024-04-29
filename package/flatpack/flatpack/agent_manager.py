c6f07c008fd500cef9a55d465dba8b15f4252fef
Romlin Group AB <hello@romlin.com>
2024-04-29 22:28:42 +0200

    Updated AgentManager

--- package/flatpack/flatpack/agent_manager.py
+++ package/flatpack/flatpack/agent_manager.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import os
 import psutil
@@ -14,30 +15,33 @@ class AgentManager:
         self.filepath = filepath
         self.load_processes()
 
-    def save_processes(self):
+    async def save_processes(self):
         with open(self.filepath, 'w') as f:
             json.dump(self.processes, f, default=str)
 
-    def load_processes(self):
+    async def load_processes(self):
         try:
             with open(self.filepath, 'r') as f:
                 self.processes = json.load(f)
-
                 for pid in list(self.processes.keys()):
                     if not psutil.pid_exists(int(pid)):
                         del self.processes[pid]
         except FileNotFoundError:
             self.processes = {}
 
-    def spawn_agent(self, script_path):
-        process = subprocess.Popen(["python", script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    async def spawn_agent(self, script_path):
+        process = await asyncio.create_subprocess_exec(
+            "python", script_path,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
         pid = process.pid
         self.processes[str(pid)] = {
             'process': process.pid,
             'start_time': datetime.now(),
             'script_path': script_path
         }
-        self.save_processes()
+        await self.save_processes()
         print(f"Spawned agent {pid} running '{script_path}'")
         return pid
 
@@ -49,14 +53,14 @@ class AgentManager:
             for pid, details in self.processes.items():
                 print(f"PID: {pid}, Script: {details['script_path']}, Start Time: {details['start_time']}")
 
-    def terminate_agent(self, pid):
+    async def terminate_agent(self, pid):
         pid = str(pid)
         if pid in self.processes:
             try:
                 os.kill(int(pid), signal.SIGTERM)  # Send SIGTERM signal to terminate the process
                 print(f"Terminated agent {pid}")
                 del self.processes[pid]
-                self.save_processes()
+                await self.save_processes()
             except OSError as e:
                 print(f"Failed to terminate agent {pid}: {e}")
         else:
