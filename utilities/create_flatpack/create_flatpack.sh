c32b1cfafd98348f762194037ad5d32d3568d785
Romlin Group AB <hello@romlin.com>
2024-05-04 22:51:34 +0200

    Updated create_flatpack.sh

--- utilities/create_flatpack/create_flatpack.sh
+++ utilities/create_flatpack/create_flatpack.sh
@@ -28,7 +28,7 @@ cp -r warehouse/template/* "warehouse/$FLATPACK_NAME/"
 
 sed -i '' "s/# template/# $FLATPACK_NAME/g" "warehouse/$FLATPACK_NAME/README.md"
 sed -i '' "s/{{model_name}}/$FLATPACK_NAME/g" "warehouse/$FLATPACK_NAME/flatpack.toml"
-sed -i '' "s/export REPO_NAME=template/export REPO_NAME=$FLATPACK_NAME/g" "warehouse/$FLATPACK_NAME/build.sh"
+sed -i '' "s/export DEFAULT_REPO_NAME=template/export DEFAULT_REPO_NAME=$FLATPACK_NAME/g" "warehouse/$FLATPACK_NAME/build.sh"
 sed -i '' "s/export FLATPACK_NAME=template/export FLATPACK_NAME=$FLATPACK_NAME/g" "warehouse/$FLATPACK_NAME/build.sh"
 
 echo "Contents of warehouse/template copied to warehouse/$FLATPACK_NAME."
