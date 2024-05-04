5d4e0003f41c90c8635e1344a514f83f2b4b9bc3
Romlin Group AB <hello@romlin.com>
2024-05-04 21:54:32 +0200

    Updated package

--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="3.4.20",
+    version="3.4.22",
     license="Apache Software License (Apache-2.0)",
     packages=find_packages(),
     install_requires=[
         "beautifulsoup4==4.12.3",
         "cryptography==42.0.6",
         "faiss-cpu==1.8.0",
-        "fastapi==0.111.0",
+        "fastapi==0.110.2",
         "hnswlib==0.8.0",
         "httpx==0.27.0",
         "huggingface-hub==0.23.0",
@@ -20,7 +20,7 @@ setup(
         "ngrok==1.2.0",
         "nltk==3.8.1",
         "olefile==0.47",
-        "psutil==5.9.5",
+        "psutil==5.9.8",
         "pydantic==2.7.1",
         "pypdf==4.2.0",
         "requests==2.31.0",
