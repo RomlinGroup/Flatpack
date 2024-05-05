bc9da01bcf0bfb1002ddf5e22362b8569984cb58
Romlin Group AB <hello@romlin.com>
2024-05-06 00:38:05 +0200

    Updated sandbox

--- warehouse/sandbox/custom.sh
+++ warehouse/sandbox/custom.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# "${VENV_PIP}"
-# "${VENV_PYTHON}"
-
 TEMP_PYTHON_SCRIPT=$(mktemp /tmp/python_script.XXXXXX.py)
+
 trap "rm -f $TEMP_PYTHON_SCRIPT" EXIT
 
 python_block() {
-    cat <<EOF >> "$TEMP_PYTHON_SCRIPT"
+  cat <<EOF >> "$TEMP_PYTHON_SCRIPT"
 $1
 EOF
 }
@@ -16,14 +14,14 @@ python_block """
 print(\"Python script execution started...\")
 
 def compute_average(numbers):
-    return sum(numbers) / len(numbers)
+  return sum(numbers) / len(numbers)
 """
 
 python_block """
 import math
 
 def compute_factorial(num):
-    return math.factorial(num)
+  return math.factorial(num)
 
 print('Factorial of 5 is:', compute_factorial(5))
 """
