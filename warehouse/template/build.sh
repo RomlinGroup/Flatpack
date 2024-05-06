8e8dd90670db252896eb20e936c3cb6bf4f9cda9
Romlin Group AB <hello@romlin.com>
2024-05-06 10:17:55 +0200

    Updated flatpacks

--- warehouse/template/build.sh
+++ warehouse/template/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Get the directory where the script is located
 export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo -e "🚀 build.sh is running in: $SCRIPT_DIR"
@@ -15,10 +14,8 @@ source "$SCRIPT_DIR/device.sh" || {
   exit 1
 }
 
-# Required devices (cpu cuda mps)
 REQUIRED_DEVICES="cuda mps"
 
-# Check if DEVICE is among the required devices
 if [[ ! " $REQUIRED_DEVICES " =~ " $DEVICE " ]]; then
   echo "😱 Error: This script requires one of the following devices: $REQUIRED_DEVICES." >&2
   exit 1
