f50bae9e1347a40b5829bc64c706d1e7b948cc5e
Romlin Group AB <dev@flatpack.ai>
2024-05-12 11:23:05 +0200

    Added init.sh

---
+++ warehouse/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+print('📦 Initializing the FPK package')
+
+TEMP_PYTHON_SCRIPT=$(mktemp /tmp/python_script.XXXXXX.py)
+VALIDATOR_SCRIPT=$(mktemp /tmp/validator_script.XXXXXX.py)
+
+trap "rm -f $TEMP_PYTHON_SCRIPT $VALIDATOR_SCRIPT" EXIT
+
+part_python() {
+  echo "$1" >> "$TEMP_PYTHON_SCRIPT"
+  echo "$1" >> "$VALIDATOR_SCRIPT"
+
+  if ! "$VENV_PYTHON" -m py_compile "$VALIDATOR_SCRIPT"; then
+    echo "❌ Invalid Python code after recent addition. Exiting..."
+    exit 1
+  fi
+}
