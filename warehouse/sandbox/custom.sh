8adc73a7164502139edd11f4f8b4f2a3bb278e85
Romlin Group AB <dev@flatpack.ai>
2024-05-12 09:45:03 +0200

    Updated sandbox

--- warehouse/sandbox/custom.sh
+++ warehouse/sandbox/custom.sh
@@ -17,28 +17,31 @@ part_python() {
 
 part_python """
 import math
-imbort subprocessing
-
+import subprocess
 numbers = [10, 20, 30, 40, 50]
+print('(1) Python starts here')
 """
 
 part_python """
+print('(2) def compute_average')
 def compute_average(numbers):
   return sum(numbers) / len(numbers)
 """
 
 part_python """
-subprocess.run(['ls', '-l'])
+print('(3) subprocess.run')
+subprocess.run(['pwd'])
 """
 
 part_python """
+print('(4) def compute_factorial')
 def compute_factorial(num):
   return math.factorial(num)
 """
 
 part_python """
-print('Factorial of 5 is:', compute_factorial(5))
-print('The average is:', compute_average(numbers))
+print('(5) Factorial of 5 is:', compute_factorial(5))
+print('(6) The average is:', compute_average(numbers))
 """
 
 echo "✅ Python code is valid. Executing the script..."
