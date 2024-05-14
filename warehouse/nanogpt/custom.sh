09a9bbedb175cfd910ee37923e0eb23b519b89ca
Romlin Group AB <dev@flatpack.ai>
2024-05-14 13:57:26 +0200

    Updated nanogpt

--- warehouse/nanogpt/custom.sh
+++ warehouse/nanogpt/custom.sh
@@ -10,7 +10,11 @@ source "$init_script" || { echo "Failed to load init.sh."; exit 1; }
 "${VENV_PIP}" install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 
 cp train.py train.py.backup
-sed -i '' "s/device = 'cuda'/device = 'mps'/" train.py
-sed -i '' 's/compile = True/compile = False/' train.py
+
+SED_CMD="sed -i"
+[[ "$OS" == "Darwin" ]] && SED_CMD="sed -i ''"
+$SED_CMD "s/device = 'cuda'/device = '$( [[ "$OS" == "Darwin" ]] && echo 'mps' || echo 'cpu' )'/" train.py
+$SED_CMD 's/compile = True/compile = False/' train.py
+
 "${VENV_PYTHON}" data/shakespeare_char/prepare.py
 "${VENV_PYTHON}" train.py config/train_shakespeare_char.py
