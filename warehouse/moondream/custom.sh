f4d4a43468df2a245876fc2e320778976e0ec1b5
Romlin Group AB <dev@flatpack.ai>
2024-05-14 14:09:21 +0200

    Updated moondream

--- warehouse/moondream/custom.sh
+++ warehouse/moondream/custom.sh
@@ -7,5 +7,7 @@ init_script="$SCRIPT_DIR/init.sh"
 [ -f "$init_script" ] || { echo "init.sh not found, exiting."; exit 1; }
 source "$init_script" || { echo "Failed to load init.sh."; exit 1; }
 
+"${VENV_PIP}" install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
+
 cp ../tiger.png tiger.png
 "${VENV_PYTHON}" sample.py --image "tiger.png" --prompt "Should I pet this dog?"
