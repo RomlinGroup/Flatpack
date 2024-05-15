d98f0101085fde573dfd64c4041992fe238c0ebf
Romlin Group AB <dev@flatpack.ai>
2024-05-15 14:10:19 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 
-# Check if lsb_release is installed
 if ! command -v lsb_release &> /dev/null; then
   echo "❌ lsb_release command not found. This script requires lsb_release to check the OS."
   exit 1
 fi
 
-# Get the operating system name
 OS=$(lsb_release -is 2>/dev/null)
 
-# Check if the operating system is Ubuntu
 if [[ "$OS" == "Ubuntu" ]]; then
   echo "🐧 Setting up Ubuntu environment"
 
-  # Add deadsnakes PPA
   if ! sudo add-apt-repository ppa:deadsnakes/ppa -y; then
     echo "❌ Failed to add deadsnakes PPA."
     exit 1
   fi
 
-  # Update package list
   if ! sudo apt-get update; then
     echo "❌ Failed to update package list."
     exit 1
   fi
 
-  # Install build-essential, Python 3.11, and related packages
   if ! sudo apt-get install -y build-essential python3.11 python3.11-dev python3.11-venv; then
     echo "❌ Failed to install necessary packages."
     exit 1
   fi
 
-  # Install pipx
   if ! sudo apt-get install -y pipx; then
     echo "❌ Failed to install pipx."
     exit 1
@@ -41,6 +34,26 @@ if [[ "$OS" == "Ubuntu" ]]; then
   if ! pipx ensurepath; then
     echo "❌ Failed to ensure pipx is in PATH."
     exit 1
+  fi
+
+  # Source the updated profile to reflect PATH changes immediately
+  if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+  elif [ -f ~/.zshrc ]; then
+    source ~/.zshrc
+  fi
+
+  # Optional: ensure pipx actions can be performed with --global argument
+  if ! sudo pipx ensurepath --global; then
+    echo "❌ Failed to ensure global pipx path."
+    exit 1
+  fi
+
+  # Source the updated profile again for global path changes
+  if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+  elif [ -f ~/.zshrc ]; then
+    source ~/.zshrc
   fi
 
   # Install flatpack using pipx with Python 3.11
