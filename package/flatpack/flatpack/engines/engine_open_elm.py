3d0d672d109ecb2c61608c3118d1c2cf2216f1ca
Romlin Group AB <dev@flatpack.ai>
2024-05-17 06:50:46 +0200

    Updated OpenELMEngine

--- package/flatpack/flatpack/engines/engine_open_elm.py
+++ package/flatpack/flatpack/engines/engine_open_elm.py
@@ -2,7 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 class OpenELMEngine:
-    def __init__(self, model_name, n_ctx=4096, n_threads=6, verbose=False, hf_access_token=None):
+    def __init__(self, model_name, n_ctx=4096, verbose=False, hf_access_token=None):
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name,
             use_auth_token=hf_access_token,
@@ -19,16 +19,16 @@ class OpenELMEngine:
         self.verbose = verbose
 
     def generate_response(self, context, question):
-        prompt = f"""
-        Context: {context}\n
-        Question: {question}\n
-        """
+        prompt = f"Context: {context}\nQuestion: {question}\n"
         inputs = self.tokenizer(prompt, return_tensors='pt')
+
         output = self.model.generate(
             input_ids=inputs['input_ids'],
             attention_mask=inputs['attention_mask'],
             max_length=self.n_ctx,
-            repetition_penalty=1.0
+            repetition_penalty=1.0,
+            pad_token_id=self.tokenizer.eos_token_id
         )
+
         response = self.tokenizer.decode(output[0], skip_special_tokens=True)
         return response
