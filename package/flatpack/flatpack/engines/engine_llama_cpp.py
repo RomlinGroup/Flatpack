682454660ba3f3bb577cd5fc5f1fc0499c693010
Romlin Group AB <dev@flatpack.ai>
2024-05-19 14:21:33 +0200

    Updated package

--- package/flatpack/flatpack/engines/engine_llama_cpp.py
+++ package/flatpack/flatpack/engines/engine_llama_cpp.py
@@ -2,16 +2,26 @@ from llama_cpp import Llama
 
 
 class LlamaCPPEngine:
-    def __init__(self, repo_id, filename, n_ctx=4096, n_threads=6, verbose=False):
-        self.model = Llama.from_pretrained(
-            repo_id=repo_id,
-            filename=filename,
-            n_ctx=n_ctx,
-            n_threads=n_threads,
-            temp=1.0,
-            repeat_penalty=1.0,
-            verbose=verbose
-        )
+    def __init__(self, repo_id=None, filename=None, n_ctx=4096, n_threads=6, verbose=False):
+        if repo_id:
+            self.model = Llama.from_pretrained(
+                repo_id=repo_id,
+                filename=filename,
+                n_ctx=n_ctx,
+                n_threads=n_threads,
+                temp=1.0,
+                repeat_penalty=1.0,
+                verbose=verbose
+            )
+        else:
+            self.model = Llama.from_local(
+                model_path=filename,
+                n_ctx=n_ctx,
+                n_threads=n_threads,
+                temp=1.0,
+                repeat_penalty=1.0,
+                verbose=verbose
+            )
 
     def generate_response(self, context, question):
         prompt = f"""
