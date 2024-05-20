9cbe48ec558fb3b4573cb83718b46872778fd44f
Romlin Group AB <dev@flatpack.ai>
2024-05-20 17:28:11 +0200

    Updated VectorManager

--- package/flatpack/flatpack/engines/engine_llama_cpp.py
+++ package/flatpack/flatpack/engines/engine_llama_cpp.py
@@ -1,13 +1,8 @@
-import time
-
 from llama_cpp import Llama
 
 
 class LlamaCPPEngine:
     def __init__(self, repo_id=None, filename=None, n_ctx=4096, n_threads=8, verbose=False):
-        start_time = time.time()
-        print("Initializing LlamaCPPEngine...")
-
         if repo_id:
             print(f"Loading model from repo_id: {repo_id}")
             self.model = Llama.from_pretrained(
@@ -36,8 +31,6 @@ class LlamaCPPEngine:
                 verbose=verbose
             )
 
-        print(f"Model loaded in {time.time() - start_time:.2f} seconds")
-
     def generate_response(self, context, question, max_tokens):
         prompt = f"""
         Context: {context}\n
@@ -48,22 +41,14 @@ class LlamaCPPEngine:
             {"role": "user", "content": prompt}
         ]
 
-        start_time = time.time()
-        print("Generating response...")
-
         response_chunks = self.model.create_chat_completion(
             max_tokens=max_tokens,
             messages=messages,
-            stop=[""],
+            stop=["<|end|>"],
             stream=True
         )
-
-        print(f"Response generation started in {time.time() - start_time:.2f} seconds")
 
         for chunk in response_chunks:
             delta = chunk['choices'][0]['delta']
             if 'content' in delta:
-                print(f"Yielding response chunk: {delta['content']}")
                 yield delta['content']
-
-        print(f"Response generation completed in {time.time() - start_time:.2f} seconds")
