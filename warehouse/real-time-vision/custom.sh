70dcdbb5101c5520ffec9d7001cd9dbe150c49c3
Romlin Group AB <dev@flatpack.ai>
2024-05-27 17:39:49 +0200

    Updated packages

--- warehouse/real-time-vision/custom.sh
+++ warehouse/real-time-vision/custom.sh
@@ -7,4 +7,4 @@ init_script="$SCRIPT_DIR/init.sh"
 [ -f "$init_script" ] || { echo "init.sh not found, exiting."; exit 1; }
 source "$init_script" || { echo "Failed to load init.sh."; exit 1; }
 
-"${VENV_PIP}" install flash_attn==2.5.8 numpy==1.24.4 Pillow==10.3.0 Requests==2.31.0 torch==2.3.0 torchvision==0.18.0 transformers==4.40.2
+"${VENV_PYTHON}" -c "print('Hello, World!')"
