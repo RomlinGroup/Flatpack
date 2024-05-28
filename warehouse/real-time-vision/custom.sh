9e4553d0948a6dae642c23956e9340bfb0c6062e
Romlin Group AB <dev@flatpack.ai>
2024-05-28 12:34:04 +0200

    Updated real-time-vision

--- warehouse/real-time-vision/custom.sh
+++ warehouse/real-time-vision/custom.sh
@@ -9,4 +9,6 @@ source "$init_script" || { echo "Failed to load init.sh."; exit 1; }
 
 "${VENV_PIP}" install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 
+cd ..
+
 "${VENV_PYTHON}" real-time-vision.py
