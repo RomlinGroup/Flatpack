0f173542473d95c44164e7993a1507a07c544e9d
Romlin Group AB <dev@flatpack.ai>
2024-05-29 07:57:01 +0200

    Updated phi-3-vision

--- warehouse/phi-3-vision/custom.sh
+++ warehouse/phi-3-vision/custom.sh
@@ -7,6 +7,8 @@ init_script="$SCRIPT_DIR/init.sh"
 [ -f "$init_script" ] || { echo "init.sh not found, exiting."; exit 1; }
 source "$init_script" || { echo "Failed to load init.sh."; exit 1; }
 
+"${VENV_PIP}" install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
+
 cd ..
 
 "${VENV_PYTHON}" gradio.py
