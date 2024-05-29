4e5a3c62eb1e6094dca5d027cb4698c84e48a059
Romlin Group AB <dev@flatpack.ai>
2024-05-29 06:47:45 +0200

    Added phi3-vision

---
+++ warehouse/phi-3-vision/custom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# "${VENV_PIP}"
+# "${VENV_PYTHON}"
+
+init_script="$SCRIPT_DIR/init.sh"
+[ -f "$init_script" ] || { echo "init.sh not found, exiting."; exit 1; }
+source "$init_script" || { echo "Failed to load init.sh."; exit 1; }
+
+"${VENV_PYTHON}" -c "print('Hello, World!')"
