f2b8655216006eec4e90ca1aae2955f079c8befe
Romlin Group AB <dev@flatpack.ai>
2024-07-08 07:14:29 +0200

    Added llama-cpp

---
+++ warehouse/llama-cpp/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo -e "[DEBUG] $(TZ=GMT date +"%Y-%m-%d %H:%M:%S")"
+echo -e "build.sh is running in: $SCRIPT_DIR"
+
+export DEFAULT_REPO_NAME=llama-cpp
+export FLATPACK_NAME=llama-cpp
+
+source "$SCRIPT_DIR/device.sh" || {
+  echo "Error: Failed to source device.sh" >&2
+  exit 1
+}
+
+REQUIRED_DEVICES="cpu cuda mps"
+
+if [[ ! $REQUIRED_DEVICES =~ (^|[[:space:]])$DEVICE($|[[:space:]]) ]]; then
+  echo "Error: This script requires one of the following devices: $REQUIRED_DEVICES." >&2
+  exit 1
+fi
+
+source "$SCRIPT_DIR/temp.sh" || {
+  echo "Error: Failed to source temp.sh" >&2
+  exit 1
+}
---
+++ warehouse/llama-cpp/custom.sh
@@ -0,0 +1,6 @@
+part_bash """
+echo \"Hello from Bash!\"
+"""
+part_python """
+print(\"Hello from Python!\")
+"""
