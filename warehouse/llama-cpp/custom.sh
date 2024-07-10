a9495cfa7d653debbb1763687d9a54683f4b688a
Romlin Group AB <dev@flatpack.ai>
2024-07-10 02:17:35 +0200

    Updated llama-cpp

--- warehouse/llama-cpp/custom.sh
+++ warehouse/llama-cpp/custom.sh
@@ -1,3 +1,3 @@
 part_bash """
-./llama-cli \<br>-m models/gemma-1.1-2b-it-Q4_K_M.gguf \<br>-p \"What is the meaning of life? Please provide your answer in one sentence.\" \<br>-n 64<br>
+./llama-cli \<br>-m models/gemma-1.1-2b-it-Q4_K_M.gguf \<br>-p \"What is the meaning of life?\" \<br>-n 64<br>
 """
