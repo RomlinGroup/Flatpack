926bbfc8db1611a61f7c00f9bdeaaae5a5285702
Romlin Group AB <dev@flatpack.ai>
2024-07-23 17:09:58 +0200

    Updated llama-cpp

--- warehouse/llama-cpp/custom.sh
+++ warehouse/llama-cpp/custom.sh
@@ -1,3 +1,22 @@
+disabled part_bash """
+echo \"Hello, World!\"
+"""
 part_bash """
-./llama-cli \<br>-m models/gemma-1.1-2b-it-Q4_K_M.gguf \<br>-p \"What is the meaning of life?\" \<br>-n 64<br>
+./llama-cli \
+-m models/gemma-1.1-2b-it-Q4_K_M.gguf \
+-p \"What is the meaning of life?\" \
+-n 64 \
+> output.txt \
+2>/dev/null
+"""
+part_bash """
+# Check output.txt
+
+if [ -f \"output.txt\" ]; then
+    echo \"🪝 Running hooks for output.txt:\"
+    echo \"----------------------\"
+    cat output.txt
+else
+    echo \"Error: output.txt does not exist.\"
+fi
 """
