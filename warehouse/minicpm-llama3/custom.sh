9c737ee91a126b465333c2aac596ff0e2917d169
Romlin Group AB <dev@flatpack.ai>
2024-07-26 22:46:34 +0200

    Updated minicpm-llama3

--- warehouse/minicpm-llama3/custom.sh
+++ warehouse/minicpm-llama3/custom.sh
@@ -1,6 +1,28 @@
-part_bash """
-echo \"Hello from Bash!\"
+disabled part_bash """
+wget https://huggingface.co/openbmb/MiniCPM-Llama3-V-2_5-gguf/resolve/main/mmproj-model-f16.gguf
 """
-part_python """
-print(\"Hello from Python!\")
+disabled part_bash """
+wget https://huggingface.co/openbmb/MiniCPM-Llama3-V-2_5-gguf/resolve/main/ggml-model-Q4_K_M.gguf
+"""
+part_bash """
+./minicpmv-cli \
+-m ggml-model-Q4_K_M.gguf \
+--mmproj mmproj-model-f16.gguf \
+-c 4096 \
+--temp 0.7 \
+--top-p 0.8 \
+--top-k 100 \
+--repeat-penalty 1.05 \
+--image tiger.png \
+-p \"Should I pet this dog?\" \
+> output.txt \
+2>/dev/null
+"""
+part_bash """
+if [ -f \"output.txt\" ]; then
+    echo \"🪝 Running hooks for output.txt:\"
+    cat output.txt
+else
+    echo \"Error: output.txt does not exist.\"
+fi
 """
