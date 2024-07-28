4ca34bebc147cf8700a1dcf6358fdcf25a93d0e7
Romlin Group AB <dev@flatpack.ai>
2024-07-28 07:58:22 +0200

    Bumped version

--- package/flatpack/setup.py
+++ package/flatpack/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="flatpack",
-    version="3.6.0",
+    version="3.6.1",
     license="Apache Software License (Apache-2.0)",
     packages=find_packages(),
     install_requires=[
@@ -13,18 +13,18 @@ setup(
         "fastapi==0.111.1",
         "hnswlib==0.8.0",
         "httpx==0.27.0",
-        "huggingface-hub==0.24.0",
+        "huggingface-hub==0.24.2",
         "ngrok==1.3.0",
         "nltk==3.8.1",
         "prettytable==3.10.2",
-        "psutil==5.9.5",
+        "psutil==6.0.0",
         "pydantic==2.8.2",
         "pypdf==4.3.1",
         "python-multipart==0.0.9",
-        "requests==2.31.0",
+        "requests==2.32.3",
         "sentence-transformers==3.0.1",
         "toml==0.10.2",
-        "torch==2.3.1",
+        "torch==2.4.0",
         "uvicorn==0.30.3"
     ],
     author="Romlin Group AB",
