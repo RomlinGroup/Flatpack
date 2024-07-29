9b025c9524d24cff3e1cdbc888b055eeeb902a0b
Romlin Group AB <dev@flatpack.ai>
2024-07-29 21:00:34 +0200

    Updated edge

--- warehouse/edge/custom.sh
+++ warehouse/edge/custom.sh
@@ -1,6 +1,49 @@
-part_bash """
-echo \"Hello from Bash!\"
+disabled part_bash """
+curl -L -o piper_arm64.tar.gz https://github.com/rhasspy/piper/releases/download/v1.2.0/piper_arm64.tar.gz
+
+tar -xzf piper_arm64.tar.gz
 """
-part_python """
-print(\"Hello from Python!\")
+disabled part_bash """
+cd piper
+
+curl -L -o en_US-lessac-medium.onnx https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx
+
+curl -L -o en_US-lessac-medium.onnx.json https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json
+"""
+disabled part_bash """
+cd piper
+echo 'The quick brown fox jumps over the lazy dog.' | \
+./piper --model en_US-lessac-medium.onnx --output_file test.wav
+"""
+disabled part_bash """
+git clone https://github.com/ggerganov/llama.cpp
+"""
+disabled part_bash """
+sudo apt update
+sudo apt install git g++ wget build-essential
+"""
+disabled part_bash """
+cd llama.cpp
+make -j
+"""
+disabled part_bash """
+curl -L -o llama.cpp/models/llama-2-7b-chat.Q2_K.gguf https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf
+"""
+part_bash """
+cd llama.cpp
+
+./llama-cli \
+-m models/llama-2-7b-chat.Q2_K.gguf \
+-p \"What is edge artificial intelligence?\" \
+-n 400 \
+> output.txt \
+2>/dev/null
+"""
+part_bash """
+if [ -f \"output.txt\" ]; then
+    echo \"Running hooks for output.txt:\"
+    cat output.txt
+else
+    echo \"Error: output.txt does not exist.\"
+fi
 """
