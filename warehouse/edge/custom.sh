0a143ac6f58128da331469b87a3b628c1aafc419
Romlin Group AB <dev@flatpack.ai>
2024-07-29 22:02:10 +0200

    Updated edge

--- warehouse/edge/custom.sh
+++ warehouse/edge/custom.sh
@@ -1,37 +1,32 @@
-disabled part_bash """
+part_bash """
 curl -L -o piper_arm64.tar.gz https://github.com/rhasspy/piper/releases/download/v1.2.0/piper_arm64.tar.gz
 
 tar -xzf piper_arm64.tar.gz
 """
-disabled part_bash """
-cd piper
+part_bash """
+curl -L -o piper/en_US-lessac-medium.onnx https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx
 
-curl -L -o en_US-lessac-medium.onnx https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx
-
-curl -L -o en_US-lessac-medium.onnx.json https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json
+curl -L -o piper/en_US-lessac-medium.onnx.json https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json
 """
-disabled part_bash """
-cd piper
+part_bash """
 echo 'The quick brown fox jumps over the lazy dog.' | \
-./piper --model en_US-lessac-medium.onnx --output_file test.wav
+./piper/piper --model en_US-lessac-medium.onnx --output_file test.wav
 """
-disabled part_bash """
+part_bash """
 git clone https://github.com/ggerganov/llama.cpp
 """
-disabled part_bash """
+part_bash """
 sudo apt update
 sudo apt install git g++ wget build-essential
 """
-disabled part_bash """
+part_bash """
 cd llama.cpp
 make -j
 """
-disabled part_bash """
-curl -L -o llama.cpp/models/llama-2-7b-chat.Q2_K.gguf https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf
+part_bash """
+curl -L -o models/llama-2-7b-chat.Q2_K.gguf https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf
 """
 part_bash """
-cd llama.cpp
-
 ./llama-cli \
 -m models/llama-2-7b-chat.Q2_K.gguf \
 -p \"What is edge artificial intelligence?\" \
