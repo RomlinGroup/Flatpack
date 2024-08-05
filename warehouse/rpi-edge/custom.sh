a5cf64431674d54b26bce08cec80def1a7cce067
Romlin Group AB <dev@flatpack.ai>
2024-08-05 08:44:06 +0200

    Debugging

--- warehouse/rpi-edge/custom.sh
+++ warehouse/rpi-edge/custom.sh
@@ -1,17 +1,5 @@
 disabled part_bash """
-sudo apt update
-sudo apt install git g++ wget build-essential
-
-curl -L -o piper_arm64.tar.gz https://github.com/rhasspy/piper/releases/download/v1.2.0/piper_arm64.tar.gz
-
-tar -xzf piper_arm64.tar.gz
-
-curl -L -o piper/en_US-lessac-medium.onnx https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx
-
-curl -L -o piper/en_US-lessac-medium.onnx.json https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json
-
 git clone https://github.com/ggerganov/llama.cpp
-
 cd llama.cpp
 make -j
 """
