d02a1a40dc0d9d876e38b239d7c5fb5759b2d480
Romlin Group AB <dev@flatpack.ai>
2024-08-06 00:05:33 +0200

    Updated rpi-edge

--- warehouse/rpi-edge/build.sh
+++ warehouse/rpi-edge/build.sh
@@ -13,7 +13,7 @@ source "$SCRIPT_DIR/device.sh" || {
   exit 1
 }
 
-REQUIRED_DEVICES="cpu"
+REQUIRED_DEVICES="cpu cuda mps"
 
 if [[ ! $REQUIRED_DEVICES =~ (^|[[:space:]])$DEVICE($|[[:space:]]) ]]; then
   echo "Error: This script requires one of the following devices: $REQUIRED_DEVICES." >&2
