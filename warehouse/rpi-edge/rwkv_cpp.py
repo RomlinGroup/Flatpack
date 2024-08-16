feae3e1c5a42b9c2779d7dbe603eaf6f84a1de05
Romlin Group AB <dev@flatpack.ai>
2024-08-17 01:08:11 +0200

    Updated flatpacks

--- warehouse/rpi-edge/rwkv_cpp.py
+++ warehouse/rpi-edge/rwkv_cpp.py
@@ -195,7 +195,7 @@ while True:
     full_response = tokenizer_decode(accumulated_tokens).strip()
 
     if full_response:
-        print(f"\n{bot}{separator} {full_response}")
+        print(f"{bot}{separator} {full_response}")
 
         print(flush=True)
 
