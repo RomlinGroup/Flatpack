a0e43b6e6c8b139d5b0c627a71fb4ae81d681fcb
Romlin Group AB <dev@flatpack.ai>
2024-09-07 00:35:14 +0200

    Debugging

--- warehouse/minicpm-s-1b/custom.sh
+++ warehouse/minicpm-s-1b/custom.sh
@@ -1,36 +1,6 @@
 part_bash """
-../bin/pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
-"""
-part_bash """
-../bin/pip install accelerate datamodel_code_generator jsonschema
+echo \"Hello from Bash!\"
 """
 part_python """
-import torch
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-path = \"openbmb/MiniCPM3-4B\"
-device = \"cpu\"
-
-tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(path, torch_dtype=torch.bfloat16, device_map=device, trust_remote_code=True)
-
-messages = [
-    {\"role\": \"user\", \"content\": \"What is the meaning of life?\"},
-]
-model_inputs = tokenizer.apply_chat_template(messages, return_tensors=\"pt\", add_generation_prompt=True).to(device)
-
-model_outputs = model.generate(
-    model_inputs,
-    max_new_tokens=1024,
-    top_p=0.7,
-    temperature=0.7
-)
-
-output_token_ids = [
-    model_outputs[i][len(model_inputs[i]):] for i in range(len(model_inputs))
-]
-
-responses = tokenizer.batch_decode(output_token_ids, skip_special_tokens=True)[0]
-print(responses)
+print(\"Hello from Python!\")
 """
