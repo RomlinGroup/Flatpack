d4c15b98564adaa0834dc7c35acb8865b1aa8bfc
Romlin Group AB <dev@flatpack.ai>
2024-09-06 21:15:07 +0200

    Updated llm-c

--- warehouse/llm-c/custom.sh
+++ warehouse/llm-c/custom.sh
@@ -1,6 +1,6 @@
 part_bash """
-echo \"Hello from Bash!\"
-"""
-part_python """
-print(\"Hello from Python!\")
+chmod u+x ./dev/download_starter_pack.sh
+./dev/download_starter_pack.sh
+make train_gpt2
+OMP_NUM_THREADS=8 ./train_gpt2
 """
