5fc0ab0a3bac491e47be5086ec78e88cdaaeca39
Romlin Group AB <dev@flatpack.ai>
2024-09-06 22:18:06 +0200

    Updated minicpm3-4b

--- warehouse/minicpm3-4b/build.sh
+++ warehouse/minicpm3-4b/build.sh
@@ -5,8 +5,8 @@ export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo -e "[DEBUG] $(TZ=GMT date +"%Y-%m-%d %H:%M:%S")"
 echo -e "build.sh is running in: $SCRIPT_DIR"
 
-export DEFAULT_REPO_NAME=minicpm-llama3
-export FLATPACK_NAME=minicpm-llama3
+export DEFAULT_REPO_NAME=minicpm3-4b
+export FLATPACK_NAME=minicpm3-4b
 
 source "$SCRIPT_DIR/device.sh" || {
   echo "Error: Failed to source device.sh" >&2
