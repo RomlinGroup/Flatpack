db1163906f9c21e011affad4df5ef8bbfd4421ee
Romlin Group AB <dev@flatpack.ai>
2024-09-07 00:41:46 +0200

    Debugging

--- warehouse/minicpm-s-1b/build.sh
+++ warehouse/minicpm-s-1b/build.sh
@@ -5,7 +5,7 @@ export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo -e "[DEBUG] $(TZ=GMT date +"%Y-%m-%d %H:%M:%S")"
 echo -e "build.sh is running in: $SCRIPT_DIR"
 
-export DEFAULT_REPO_NAME=minicpm-s-1b
+export DEFAULT_REPO_NAME=PowerInfer
 export FLATPACK_NAME=minicpm-s-1b
 
 source "$SCRIPT_DIR/device.sh" || {
