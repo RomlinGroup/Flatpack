c991771fd79410d076e136ddd69c7123041701c9
Romlin Group AB <dev@flatpack.ai>
2024-09-07 11:04:09 +0200

    Updated nanogpt

--- warehouse/nanogpt/custom.sh
+++ warehouse/nanogpt/custom.sh
@@ -7,3 +7,6 @@ part_bash """
 part_bash """
 ../bin/python train.py config/train_shakespeare_char.py --device=cpu --compile=False --eval_iters=20 --log_interval=1 --block_size=64 --batch_size=12 --n_layer=4 --n_head=4 --n_embd=128 --max_iters=2000 --lr_decay_iters=2000 --dropout=0.0
 """
+part_bash """
+../bin/python sample.py --out_dir=out-shakespeare-char --device=cpu
+"""
