c09c93263ed7f062099dd2516286853b935e3065
Romlin Group AB <dev@flatpack.ai>
2024-09-07 20:43:29 +0200

    Updated agent-rag

--- warehouse/agent-rag/custom.sh
+++ warehouse/agent-rag/custom.sh
@@ -1,8 +1,6 @@
 part_bash """
-wget -nc -q https://raw.githubusercontent.com/RomlinGroup/Flatpack/main/agents/fast_api_local.py
-
-wget -nc -q https://huggingface.co/ggml-org/gemma-1.1-2b-it-Q4_K_M-GGUF/resolve/main/gemma-1.1-2b-it.Q4_K_M.gguf
+echo \"Hello from Bash!\"
 """
-part_bash """
-../bin/flatpack agents spawn fast_api_local.py
+part_python """
+print(\"Hello from Python!\")
 """
