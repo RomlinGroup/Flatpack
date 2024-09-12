3b35d8cb949e73f0a4d9a1f30135beabde2ba207
Romlin Group AB <dev@flatpack.ai>
2024-09-13 01:14:29 +0200

    Updated bark-local

--- warehouse/bark-local/custom.sh
+++ warehouse/bark-local/custom.sh
@@ -1,6 +1,21 @@
 part_bash """
-echo \"Hello from Bash!\"
+../bin/pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 """
 part_python """
-print(\"Hello from Python!\")
+import scipy
+
+from transformers import AutoProcessor, BarkModel
+
+processor = AutoProcessor.from_pretrained(\"suno/bark\")
+
+model = BarkModel.from_pretrained(\"suno/bark-small\")
+
+inputs = processor(\"\"\" What is the meaning of life? \"\"\")
+
+audio_array = model.generate(**inputs)
+audio_array = audio_array.cpu().numpy().squeeze()
+
+sample_rate = model.generation_config.sample_rate
+
+scipy.io.wavfile.write(\"output.wav\",rate=sample_rate, data=audio_array)
 """
