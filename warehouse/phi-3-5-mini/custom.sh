22e7c1747be531f27820c69a3675c66ee54e9a1e
Romlin Group AB <dev@flatpack.ai>
2024-09-14 11:40:57 +0200

    Updated phi-3-5-mini

--- warehouse/phi-3-5-mini/custom.sh
+++ warehouse/phi-3-5-mini/custom.sh
@@ -1,6 +1,3 @@
 part_bash """
-echo \"Hello from Bash!\"
-"""
-part_python """
-print(\"Hello from Python!\")
+../bin/pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 """
