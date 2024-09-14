6f61a77f4e04f821241996cddeca8b9e662025f5
Romlin Group AB <dev@flatpack.ai>
2024-09-14 10:39:41 +0200

    Updated phi-3-5-mini

--- warehouse/phi-3-5-mini/build.sh
+++ warehouse/phi-3-5-mini/build.sh
@@ -5,7 +5,7 @@ export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo -e "[DEBUG] $(TZ=GMT date +"%Y-%m-%d %H:%M:%S")"
 echo -e "build.sh is running in: $SCRIPT_DIR"
 
-export DEFAULT_REPO_NAME=llama.cpp
+export DEFAULT_REPO_NAME=phi-3-5-mini
 export FLATPACK_NAME=phi-3-5-mini
 
 source "$SCRIPT_DIR/device.sh" || {
