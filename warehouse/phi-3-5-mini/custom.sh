3df104cb49eeced72a3dcbd9b27e85863ae8208f
Romlin Group AB <dev@flatpack.ai>
2024-09-19 07:35:15 +0200

    Updated phi-3-5-mini

--- warehouse/phi-3-5-mini/custom.sh
+++ warehouse/phi-3-5-mini/custom.sh
@@ -1,3 +1,22 @@
 part_bash """
-../bin/pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
+if [ ! -f models/Phi-3.5-mini-instruct-Q4_K_S.gguf ]; then
+    wget -nc -O models/Phi-3.5-mini-instruct-Q4_K_S.gguf \"https://huggingface.co/bartowski/Phi-3.5-mini-instruct-GGUF/resolve/main/Phi-3.5-mini-instruct-Q4_K_S.gguf\"
+else
+    echo \"Model already exist.\"
+fi
+"""
+part_bash """
+./llama-cli \
+-m models/Phi-3.5-mini-instruct-Q4_K_S.gguf \
+-n 64 \
+-p \"What is the meaning of life?\" \
+> output.txt \
+2>log.txt
+"""
+part_bash """
+if [ -f \"output.txt\" ]; then
+    cat output.txt
+else
+    echo \"Error: output.txt does not exist.\"
+fi
 """
