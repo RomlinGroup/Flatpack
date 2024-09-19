c2fe80486165fa93b1431180c83ac740b23cfb8e
Romlin Group AB <dev@flatpack.ai>
2024-09-20 00:04:53 +0200

    Debugging

--- package/flatpack/flatpack/error_handling.py
+++ package/flatpack/flatpack/error_handling.py
@@ -1,3 +1,4 @@
+import asyncio
 import signal
 import sys
 import traceback
@@ -11,7 +12,6 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     else:
         sys.__stderr__.write("An unexpected error occurred:\n")
         traceback.print_exception(exc_type, exc_value, exc_traceback)
-    sys.exit(1)
 
 
 def setup_exception_handling():
@@ -19,7 +19,7 @@ def setup_exception_handling():
 
 
 def sigint_handler(signum, frame):
-    sys.exit(0)
+    raise KeyboardInterrupt()
 
 
 def setup_signal_handling():
@@ -30,12 +30,37 @@ def safe_exit(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            if asyncio.iscoroutinefunction(func):
+                return asyncio.run(func(*args, **kwargs))
+            else:
+                return func(*args, **kwargs)
         except SystemExit:
             sys.__stderr__.write("Exiting the program...\n")
             sys.exit(0)
+        except KeyboardInterrupt:
+            sys.__stderr__.write("\nProgram interrupted by user. Exiting gracefully...\n")
+            sys.exit(0)
         except Exception as e:
             sys.__stderr__.write(f"An unexpected error occurred: {str(e)}\n")
+            traceback.print_exc()
             sys.exit(1)
 
     return wrapper
+
+
+async def shutdown(signal, loop):
+    """Cleanup tasks tied to the service's shutdown."""
+    sys.__stderr__.write(f"Received exit signal {signal.name}...\n")
+    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+
+    for task in tasks:
+        task.cancel()
+
+    sys.__stderr__.write(f"Cancelling {len(tasks)} outstanding tasks\n")
+    await asyncio.gather(*tasks, return_exceptions=True)
+    loop.stop()
+
+
+def handle_asyncio_exception(loop, context):
+    msg = context.get("exception", context["message"])
+    sys.__stderr__.write(f"Caught asyncio exception: {msg}\n")
