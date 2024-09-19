697786f84bf3e885d8a8a94ef7496100f7a9b7d7
Romlin Group AB <dev@flatpack.ai>
2024-09-19 14:43:55 +0200

    Debugging

--- warehouse/phi-3-mlx/custom.sh
+++ warehouse/phi-3-mlx/custom.sh
@@ -1,6 +1,6 @@
 part_bash """
-echo \"Hello from Bash!\"
+../bin/pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 """
-part_python """
-print(\"Hello from Python!\")
+part_bash """
+generate(\'What is shown in this image?\', \'https://collectionapi.metmuseum.org/api/collection/v1/iiif/344291/725918/main-image\')
 """
