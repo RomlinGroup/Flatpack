109b63563d55c2284a7935308362fd655091c84d
Romlin Group AB <dev@flatpack.ai>
2024-11-17 02:23:06 +0100

    Updated package

--- package/flatpack/flatpack/vector_manager.py
+++ package/flatpack/flatpack/vector_manager.py
@@ -11,6 +11,7 @@ import time
 import warnings
 
 from bs4 import BeautifulSoup
+from contextlib import redirect_stdout
 from pypdf import PdfReader
 from rich.console import Console
 from rich.progress import Progress, SpinnerColumn, TextColumn
